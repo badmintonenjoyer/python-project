35c31c7dda1c63c548de80b1606f1dfd6a23e7cd
Szymon Rochowicz <wrx86272@student.wroclaw.merito.pl>
2025-06-26 12:39:47 +0200

    Zmieniono port w main.py

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
@@ -11,4 +12,5 @@ def health():
     return "OK", 200
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000)
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port)
