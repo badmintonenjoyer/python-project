003796ab53e186e3705979c5a3000ca1f36737c9
Szymon Rochowicz <wrx86272@student.wroclaw.merito.pl>
2025-07-01 22:11:39 +0200

    proba polaczenia z render

--- main.py
+++ main.py
@@ -12,5 +12,5 @@ def health():
     return "OK", 200
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8181))
+    port = int(os.environ.get("PORT", 8000))
     app.run(host="0.0.0.0", port=port)
